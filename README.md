d3625524ba6fb8bb32110e356d3e3260e9a2325e
Margot L <mlavitt9@gmail.com>
2024-12-03 16:12:49 -0800

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BioE131_EJM_Final
 UC Berkeley BioE c131 Final Project
+Database for populating Jbrowse with annotated Herpesvirus genomes
---
+++ load_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## declare an array variable 
+declare -a arr=(“GCF_000859985.2” “GCA_027936265.1” “GCF_000858285.1” “GCF_000848845.1” “GCF_000839725.1” “GCF_000840765.1” “GCF_000845245.1” “GCF_008792765.1” “GCF_000848125.1” “GCF_002402265.1” “GCF_000838265.1” “GCF_000839325.1” “GCF_000846065.1”) 
+## now loop through the above array 
+for i in "${arr[@]}" 
+do 
+    echo "$i"
+    datasets download genome accession “$i” --include gff3,genome
+
+    add-track genes.gff --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames “$i”
+
+    jbrowse text-index --out $APACHE_ROOT/jbrowse2
+
+done
