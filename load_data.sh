30008e91546b1423a0e4a63c2487cc7b6157f6a8
Margot L <mlavitt9@gmail.com>
2024-12-05 16:26:39 -0800

    dataset inputfile accesion

--- load_data.sh
+++ load_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 ## declare an array variable 
 declare -a arr=(GCF_000859985.2 GCA_027936265.1 GCF_000858285.1 GCF_000848845.1 GCF_000839725.1 GCF_000840765.1 GCF_000845245.1 GCF_008792765.1 GCF_000848125.1 GCF_002402265.1 GCF_000838265.1 GCF_000839325.1 GCF_000846065.1) 
+
+datasets download genome accession --inputfile accessions.txt --include gff3,genome
+unzip ncbi_dataset.zip
+
 ## now loop through the above array 
 for i in "${arr[@]}" 
 do 
     echo "$i $i"
-    echo $i
-    datasets download genome accession "$i" --include gff3,genome
-
-    unzip ncbi_dataset.zip
-    echo ls ncbi_dataset/data/$i/*genomic.fna
+    echo $i    
     samtools faidx ncbi_dataset/data/$i/*genomic.fna
     jbrowse add-assembly ncbi_dataset/data/$i/*genomic.fna --out $APACHE_ROOT/jbrowse2 --type indexedFasta --load copy
     
