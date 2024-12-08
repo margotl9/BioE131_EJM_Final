9ec28965ee528e276d93a1b0380dbe12a8744e9a
Margot L <mlavitt9@gmail.com>
2024-12-08 13:37:14 -0800

    load

--- load_data.sh
+++ load_data.sh
@@ -13,9 +13,10 @@ do
     
     jbrowse add-assembly ncbi_dataset/data/$i/*genomic.fna  -n "$i" --out $APACHE_ROOT/jbrowse2 --type indexedFasta --load copy --force
     
-    jbrowse sort-gff ncbi_dataset/data/$i/genomic.gff | bgzip > ncbi_dataset/data/$i/genomic_sorted.gff.gz
-    tabix ncbi_dataset/data/$i/genomic_sorted.gff.gz
+    # jbrowse sort-gff ncbi_dataset/data/$i/genomic.gff | bgzip > ncbi_dataset/data/$i/genomic_sorted.gff.gz
+    # tabix ncbi_dataset/data/$i/genomic_sorted.gff.gz
 
-    jbrowse add-track ncbi_dataset/data/$i/genomic_sorted.gff.gz --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames "$i" --trackId "$i" --force --overwrite
+    jbrowse add-track ncbi_dataset/data/$i/genomic.gff --out $APACHE_ROOT/jbrowse2 --load copy --assemblyNames "$i" --trackId "$i" --force --overwrite
+    jbrowse text-index --out $APACHE_ROOT/jbrowse2
+
 done 
-jbrowse text-index --out $APACHE_ROOT/jbrowse2
